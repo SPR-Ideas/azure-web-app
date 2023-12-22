4f46e8dac46fe3979d27fe3d85777cf421b236ba
Pradeep Ragul S <pradeepraguls@presidio.com>
2023-12-22 21:06:44 +0530

    Added Db connection

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 
 from fastapi import FastAPI
 import uvicorn
-import pyobdc
+# import pyobdc
 
 app = FastAPI()
 
@@ -16,36 +16,36 @@ password = 'spr12345spr_A'
 
 
 
-def connect_to_db():
+# def connect_to_db():
 
-    # Construct the connection string
-    connection_string = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}'
-    status = False
-    # Try to establish a connection
-    try:
-        # Establish the database connection
-        connection = pyodbc.connect(connection_string)
+#     # Construct the connection string
+#     connection_string = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}'
+#     status = False
+#     # Try to establish a connection
+#     try:
+#         # Establish the database connection
+#         connection = pyodbc.connect(connection_string)
 
-        # Create a cursor from the connection
-        cursor = connection.cursor()
+#         # Create a cursor from the connection
+#         cursor = connection.cursor()
 
-        # Example: Execute a simple SQL query
-        cursor.execute("SELECT @@version;")
-        row = cursor.fetchone()
+#         # Example: Execute a simple SQL query
+#         cursor.execute("SELECT @@version;")
+#         row = cursor.fetchone()
 
-        # Display the result
-        print("Connected to SQL Server. Server version:", row[0])
-        status = True
-    except pyodbc.Error as e:
-        # Handle any connection errors
-        print("Error connecting to SQL Server:", e)
-        status = False
-    finally:
-        # Close the connection, whether successful or not
-        if connection:
-            connection.close()
-            print("Connection closed.")
-    return status
+#         # Display the result
+#         print("Connected to SQL Server. Server version:", row[0])
+#         status = True
+#     except pyodbc.Error as e:
+#         # Handle any connection errors
+#         print("Error connecting to SQL Server:", e)
+#         status = False
+#     finally:
+#         # Close the connection, whether successful or not
+#         if connection:
+#             connection.close()
+#             print("Connection closed.")
+#     return status
 
 @app.get("/heath-check")
 def health_check():
