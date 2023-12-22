8758085e3e7f6bc9fc2e07caae0d03ab9e3293dc
Pradeep Ragul S <pradeepraguls@presidio.com>
2023-12-22 19:16:30 +0530

    first-commit

--- main.py
+++ main.py
@@ -4,8 +4,47 @@
 
 from fastapi import FastAPI
 import uvicorn
+import pyobdc
 
 app = FastAPI()
+
+# Set your database connection parameters
+server = 'your_server_name'
+database = 'your_database_name'
+username = 'your_username'
+password = 'your_password'
+
+
+
+def connect_to_db():
+
+    # Construct the connection string
+    connection_string = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}'
+
+    # Try to establish a connection
+    try:
+        # Establish the database connection
+        connection = pyodbc.connect(connection_string)
+
+        # Create a cursor from the connection
+        cursor = connection.cursor()
+
+        # Example: Execute a simple SQL query
+        cursor.execute("SELECT @@version;")
+        row = cursor.fetchone()
+
+        # Display the result
+        print("Connected to SQL Server. Server version:", row[0])
+
+    except pyodbc.Error as e:
+        # Handle any connection errors
+        print("Error connecting to SQL Server:", e)
+
+    finally:
+        # Close the connection, whether successful or not
+        if connection:
+            connection.close()
+            print("Connection closed.")
 
 
 @app.get("/heath-check")
